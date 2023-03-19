c448da4186498697754d484cdd7280e0e6d9cd9d
Dhruv Modi <dhruvmodi428@gmail.com>
2023-03-19 11:21:36 +0530

    webpack config

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './style.css';
+
+document.getElementsByTagName("body")[0].append(document.createElement("h1").innerHTML = "yoyoyo")
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+module.exports = {
+  mode: 'production',
+  entry: {
+        index:'./src/index.js',
+        sub:'./src/sub.js'
+    },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: 'dist',
+  },
+  watch: true,
+};
